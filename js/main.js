b2f0e0b7d088281bac3be7d98f4bcaf23b75a14c
DanilWeda <draganevr@gmail.com>
2021-11-04 20:01:24 +0200

    Fix: Close Functions

--- js/main.js
+++ js/main.js
@@ -1,38 +1,34 @@
 const HTML_OBJECT = {
-	btnOne: document.getElementById('btn-one'),
-	pOne: document.getElementById('return-one'),
-	btnTwo: document.getElementById('btn-two'),
-	pTwo: document.getElementById('return-two'),
-	btnThree: document.getElementById('btn-three'),
-	pThree: document.getElementById('return-three'),
+  btnOne: document.getElementById('btn-one'),
+  pOne: document.getElementById('return-one'),
+  btnTwo: document.getElementById('btn-two'),
+  pTwo: document.getElementById('return-two'),
+  btnThree: document.getElementById('btn-three'),
+  pThree: document.getElementById('return-three'),
 }
 
-let arrayOne = [];
-let arrayTwo = [];
-let arrayThree = [];
+const countMark = () => {
+  let a = 0;
+  return function () {
+    a++;
+    return a;
+  };
+};
 
-HTML_OBJECT.btnOne.addEventListener("click",() => {
-	let testOne = test();
-	arrayOne.push(testOne())
-	HTML_OBJECT.pOne.innerText = arrayOne.length;
-})
+const TEST_COUNT = {
+  pOne: countMark(),
+  pTwo: countMark(),
+  PThree: countMark(),
+};
 
-HTML_OBJECT.btnTwo.addEventListener("click",() => {
-	let testTwo = test();
-	arrayTwo.push(testTwo())
-	HTML_OBJECT.pTwo.innerText = arrayTwo.length;
-})
+HTML_OBJECT.btnOne.addEventListener('click', () => {
+  HTML_OBJECT.pOne.innerText = TEST_COUNT.pOne();
+});
 
-HTML_OBJECT.btnThree.addEventListener("click",() => {
-	let testThree = test();
-	arrayThree.push(testThree())
-	HTML_OBJECT.pThree.innerText = arrayThree.length;
-})
+HTML_OBJECT.btnTwo.addEventListener('click', () => {
+  HTML_OBJECT.pTwo.innerText = TEST_COUNT.pTwo();
+});
 
-function test () {
-	a = 0;
-	return function() {
-		a++
-		return a;
-	}
-}
+HTML_OBJECT.btnThree.addEventListener('click', () => {
+  HTML_OBJECT.pThree.innerText = TEST_COUNT.pTwo();
+});
